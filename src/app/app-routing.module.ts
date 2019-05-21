803ea565fe747558118e6b3298972c57c8267275
Aydogdy <ashahyrov@gmail.com>
2019-05-20 21:16:51 -0500

    init && configured lazy loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: '../app/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'products',
+    loadChildren: '../app/products/products.module#ProductsModule'
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
